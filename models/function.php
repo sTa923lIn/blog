04f46f264d5d7965f7d4b9e1d11953adde748fd1
sTa923lIn <serega-stalin@i.ua>
2016-10-22 23:21:56 +0300

    создаем вьюшку

---
+++ models/function.php
@@ -0,0 +1,19 @@
+<?php
+function Articles_all(){
+
+}
+
+function Articles_get($id){
+	
+}
+
+function Articles_new($title,$date,$content){
+	
+}
+
+function Articles_delete($id,$title,$date,$content){
+	
+}
+
+
+  ?>
---
+++ views/articles_view.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<meta charset="UTF-8">
+	<title>Мой первый блог</title>
+</head>
+<body>
+	<div class="container">
+		<h1>Мой первый блог</h1>
+			<div>
+					<div class="art">
+						<h3></h3>	
+						<em></em>
+						<p></p>
+					</div>
+			</div>
+	</div>
+<div>
+		<footer>Мой  первый блог <br>
+		&copy; 2015</footer>
+</div>
+
+</body>
+</html>
