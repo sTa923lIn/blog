ab2055f66eb686d7d9328bc248ac63ec6a7f4f41
sTa923lIn <serega-stalin@i.ua>
2016-11-13 23:34:13 +0200

    создал вьшку для редактирования

--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,11 @@
 <?php  
 
 include_once('../db/database.php');
-include_once('../models/fuction.php');
+include_once('../models/function.php');
 
 	connect(); // подключаем базу данных
 	$art=Articles_all(); // присвоил переменной выборку из таблицы blog_tabel
 
-include ('../views/admin_panel.php');
+include ('../views/panel_view.php');
 
 ?>
