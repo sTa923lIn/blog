a8bc6707cb63e11bc3d4d3757a1e7107cc33fdbf
sTa923lIn <serega-stalin@i.ua>
2016-11-13 23:10:02 +0200

    создал вьюшку админ панели

--- admin/index.php
+++ admin/index.php
@@ -3,4 +3,9 @@
 include_once('../db/database.php');
 include_once('../models/fuction.php');
 
+	connect(); // подключаем базу данных
+	$art=Articles_all(); // присвоил переменной выборку из таблицы blog_tabel
+
+include ('../views/admin_panel.php');
+
 ?>
